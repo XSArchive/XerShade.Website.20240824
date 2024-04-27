b05f5a61e484588bc8a8113ef24635753c79a7cb
XerShade <xershade.ca@gmail.com>
2024-04-27 08:01:54 -0400

    Added RWDService base class.

---
+++ source/modules/XerShade.Website.Core/Services/Interfaces/IRWDService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace XerShade.Website.Core.Services.Interfaces;
+
+public interface IRWDService<TDataType> where TDataType : class
+{
+    TDataType? Read(Expression<Func<TDataType, bool>> predicate);
+    IQueryable<TDataType>? ReadAll();
+    void Write(Expression<Func<TDataType, bool>> predicate, Action<TDataType> writeAction);
+    void Delete(Expression<Func<TDataType, bool>> predicate);
+
+    Task<TDataType?> ReadAsync(Expression<Func<TDataType, bool>> predicate);
+    Task<IQueryable<TDataType>?> ReadAllAsync();
+    Task WriteAsync(Expression<Func<TDataType, bool>> predicate, Action<TDataType> writeAction);
+    Task DeleteAsync(Expression<Func<TDataType, bool>> predicate);
+}
