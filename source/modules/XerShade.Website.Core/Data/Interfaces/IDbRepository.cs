37b8977cb845bfb89fe4ebf98cc1f2df1980951c
XerShade <xershade.ca@gmail.com>
2024-05-27 16:54:20 -0400

    Revert "Cleanup IDbRepository"
    This reverts commit 5301ef7b859fc56e8f2611b774362dc2a0ebce54.

--- source/modules/XerShade.Website.Core/Data/Interfaces/IDbRepository.cs
+++ source/modules/XerShade.Website.Core/Data/Interfaces/IDbRepository.cs
@@ -1,17 +1,19 @@
-﻿using System.Linq.Expressions;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace XerShade.Website.Core.Data.Interfaces;
+
 public interface IDbRepository<DataType> where DataType : class
 {
-    Task AddAsync(DataType entity);
-    Task AddAsync(IEnumerable<DataType> entities);
-    Task<IEnumerable<DataType>> GetAllAsync();
     Task<DataType> GetAsync(Expression<Func<DataType, bool>> predicate);
     Task<DataType> GetByIdAsync(int id);
+    Task<IEnumerable<DataType>> GetAllAsync();
     Task<IEnumerable<DataType>> GetRangeAsync(Expression<Func<DataType, bool>> predicate);
+    Task AddAsync(DataType entity);
+    Task AddAsync(IEnumerable<DataType> entities);
     Task RemoveAsync(DataType entity);
-    Task RemoveAsync(Expression<Func<DataType, bool>> predicate);
-    Task RemoveAsync(IEnumerable<DataType> entities);
     Task RemoveAsync(int id);
+    Task RemoveAsync(IEnumerable<DataType> entities);
+    Task RemoveAsync(Expression<Func<DataType, bool>> predicate);
     Task SaveChangesAsync();
 }
