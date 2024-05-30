e319c8f3be90211e6cbe272688f784a86f578dfd
XerShade <xershade.ca@gmail.com>
2024-05-30 10:41:45 -0400

    Simplified option data model.

--- source/modules/XerShade.Website.Core/Data/Models/Option.cs
+++ source/modules/XerShade.Website.Core/Data/Models/Option.cs
@@ -11,29 +11,7 @@ public class Option
     [Required]
     public int OptionId { get; set; }
     [Required]
-    public string OptionName { get; set; }
-    public string OptionValue { get; set; }
-    public bool AutoLoad { get; set; }
-
-    public Option()
-    {
-        this.OptionName = string.Empty;
-        this.OptionValue = string.Empty;
-        this.AutoLoad = false;
-    }
-
-    public Option(string optionName, string optionValue, bool autoLoad)
-    {
-        this.OptionName = optionName ?? throw new ArgumentNullException(nameof(optionName));
-        this.OptionValue = optionValue;
-        this.AutoLoad = autoLoad;
-    }
-
-    public Option(int optionId, string optionName, string optionValue, bool autoLoad)
-    {
-        this.OptionId = optionId;
-        this.OptionName = optionName ?? throw new ArgumentNullException(nameof(optionName));
-        this.OptionValue = optionValue;
-        this.AutoLoad = autoLoad;
-    }
+    public string OptionName { get; set; } = string.Empty;
+    public string OptionValue { get; set; } = string.Empty;
+    public bool AutoLoad { get; set; } = false;
 }
